be7ebca9f6cd3e20f26a0af42691ef361731f28c
Gblanquez <hellogabrielblanquez@gmail.com>
2026-06-18 15:41:50 +0200

    change dup

--- src/components/scroll.js
+++ src/components/scroll.js
@@ -83,6 +83,11 @@ function scrollToTop(options = {}) {
     offset: 0,
     force: true,
     duration: 1.6,
+    easing: (t) => (
+      t < 0.5
+        ? 4 * t * t * t
+        : 1 - Math.pow(-2 * t + 2, 3) / 2
+    ),
     ...options,
   })
 
